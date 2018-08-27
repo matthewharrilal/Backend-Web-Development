58875657bfbc6b6ed93de14152f70e1763bb47ab
Matthew Harrilal <matthewharrilal@gmail.com>
2018-08-27 11:13:00 -0700

    Can now use url parameters in the route

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ app.set('view engine', 'handlebars');
 app.get('/hello-world', function(req, res) {
     // Get request to the /hello-world route
     var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
-    res.render('hello-gif', {gifUrl: gifUrl});
+    res.render('hello-gif', {gifUrl: gifUrl}); // Renders html template img tag in the template
 });
 
+app.get('/greetings/:name/', function(req,res) {
+    var name = req.params.name
+    res.render('greetings', {name:name})
+})
 app.listen(3000, function () {
     // Listen on port 3000
   console.log('Gif Search listening on port localhost:3000!');
