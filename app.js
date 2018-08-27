57e0ba7f46d2f75aa4664080c55640915d661946
Matthew Harrilal <matthewharrilal@gmail.com>
2018-08-27 10:45:32 -0700

    Changed route to hit gify url and added template

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.get('/hello-world', function(req, res) {
     // Get request to the /hello-world route
-    res.send('Hello World');
+    var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
+    res.render('hello-gif', {gifUrl: gifUrl});
 });
 
 app.listen(3000, function () {
