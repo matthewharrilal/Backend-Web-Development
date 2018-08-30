82d0d308ae1cf5f3fcfdfb9044934424322387fd
Matthew Harrilal <matthewharrilal@gmail.com>
2018-08-30 14:49:57 -0700

    Refactored to support the giphy api

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var exphbs = require('express-handlebars')
+var giphy = require('giphy-api')();
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
@@ -21,25 +22,30 @@ app.get('/', function(req, res) {
     var queryString = req.query.term
     console.log(queryString)
 
-    // Now enocode the query string as an uri component to remove whitespaces and extra characters
-    var term = encodeURIComponent(queryString)
-    // PUT THE SEARCH TERM INTO THE GIPHY API SEARCH URL
-    var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
+    // // Now enocode the query string as an uri component to remove whitespaces and extra characters
+    // var term = encodeURIComponent(queryString)
+    // // PUT THE SEARCH TERM INTO THE GIPHY API SEARCH URL
+    // var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
+    //
+    // // Making the http request
+    // http.get(url, function(response) {
+    //     // Set encoding of response to utf 8
+    //     var body = '';
+    //     response.on('data', function(d) { // When you get the data
+    //         // Continously update body with data from giphy
+    //         body += d
+    //     });
+    //
+    //     response.on('end', function() {  // When the data is fully recieved
+    //         var parsed = JSON.parse(body) // Convert data into JSON
+    //         res.render('home', {gifs: parsed.data})
+    //     });
+    // });
 
-    // Making the http request
-    http.get(url, function(response) {
-        // Set encoding of response to utf 8
-        var body = '';
-        response.on('data', function(d) { // When you get the data
-            // Continously update body with data from giphy
-            body += d
-        });
-
-        response.on('end', function() {  // When the data is fully recieved
-            var parsed = JSON.parse(body) // Convert data into JSON
-            res.render('home', {gifs: parsed.data})
-        });
-    });
+    var term = req.query.term || 'funny cat'
+    giphy.search(term, function (err, response) {
+   res.render('home', {gifs: response.data})
+ });
 });
 
 app.get('/greetings/:name/', function(req, res) {
